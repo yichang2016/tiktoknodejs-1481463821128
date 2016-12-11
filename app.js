2de3ef3a478e490b4b8f2b3149d36872f220b03b
dlycliu <dlycliu@cn.ibm.com>
2016-12-11 22:42:22 +0800

    init

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
@@ -23,6 +26,23 @@ var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+    // print a message when the server starts listening
+    console.log("server starting on " + appEnv.url);
+});
+
+io.on("connection", function(socket) {
+
+    socket.on("String", function(key) {
+        if (key == "w" || key == "s" || key == "a" || key == "d") {
+            console.log("Received " + key + " from frontend");
+
+            //************************
+            //fill in a method "io.sockets.emit(param1,param2)" in line 35
+            //The first parameter is the data type "message", the second parameter is the data you want to send
+            io.sockets.emit('message', key);
+        } else {
+            console.log("Not a direction order");
+        }
+    });
+
 });
